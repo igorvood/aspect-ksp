1902e1b3a76396107225345ea8f28fbe57fbef00
vood <igor-vood@gmail.com>
2025-08-25 16:44:32 +0300

    1

--- mode-symbol-processor/build/resources/test/ru/vood/processor/datamodel/DataModelConfigProcessorProviderTest/Deal.kt
+++
@@ -1,9 +0,0 @@
-import ru.vood.dmgen.annotation.*
-
-@Comment("Это сущность Сделка")
-@FlowEntity(FlowEntityType.AGGREGATE)
-interface Deal {
-    @Pk
-    @Comment("Это ее идентификатор")
-    val id: Int
-}
